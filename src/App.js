4afde52ad7761b197b9e3f6a12cf406ba82c10c5
Chudier <chudierp@gmail.com>
2021-05-09 09:50:51 -0700

    added list of timers

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
+import React from 'react';
 import './App.css';
-
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducers from './reducers';
+// import new components
+import NewTimer from './components/NewTimer'
+import ListTimers from './components/ListTimers'
+import './App.css';
 
 const store = createStore(reducers);
 
 function App() {
   return (
     <Provider store={store}>
-      <h1>TMRZ</h1>
+      <div className="App">
+        <h1>TMRZ</h1>
+        {/* Display the new components */}
+        <NewTimer />
+        <ListTimers />
+      </div>  
     </Provider>
   );
 }
--- src/components/NewTimer.js
+++ src/components/NewTimer.js
@@ -1,13 +1,14 @@
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { addTimer } from '../actions'
+import './NewTimer.css'
 
 export default function NewTimer() {
 	const [ name, setName ] = useState('')
 	const dispatch = useDispatch()
 
 	return (
-		<div>
+		<div className="NewTimer">
 			<input type='text' placeholder="New Timer Name" name="name" value={name} onChange={(e) => setName(e.target.value)}/>
 			<button onClick={() => dispatch(addTimer(name))}>Save</button>
 		</div>
